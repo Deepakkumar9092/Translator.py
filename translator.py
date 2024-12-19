8e78d25e01b1a3455916fcd19a8c74a8f3e99fc5
Deepak Kumar <157194485+Deepakkumar9092@users.noreply.github.com>
2024-12-19 11:30:14 +0530

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,20 +1,7 @@
-from tkinter import *
-from tkinter import ttk
+Multi-Language Translator App
+This app, developed using Python, supports translation across multiple languages. It leverages Python modules like googletrans or other translation APIs to provide seamless and accurate language conversion. The app features:
 
-root = Tk()
-root.title("Deepak Translator")
-root.geometry("500x500")
-root.config(bg="black")
-
-lab_txt = Label(root,text="Translator",font =("Time New Roman",40,"bold"))
-lab_txt.place(x=100,y=40,height=50,width=300)
-
-frame = Frame(root).pack(side = BOTTOM)
-
-lab_txt = Label(root,text="Source Text",font =("Time New Roman",20,"bold"),fg="White",bg="Black")
-lab_txt.place(x=100,y=100,height=20,width=300)
-
-Sor_txt = Text(frame,font=("Time New Roman", 20, "bold"),wrap= WORD)
-Sor_txt.place(x=10,y=130,height=200,width=480)
-
-root.mainloop()
+Input Flexibility: Users can input text in one language and translate it into another of their choice.
+Multi-Language Support: Includes a wide range of languages to cater to diverse user needs.
+User-Friendly Interface: Simplifies interaction for a smooth user experience.
+This project demonstrates effective utilization of Python's library ecosystem and a strong understanding of language processing concepts.
